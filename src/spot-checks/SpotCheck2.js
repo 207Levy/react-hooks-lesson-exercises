166731e91587c0a626d1c02f784ee2f60c111475
Or <orlevy11@gmail.com>
2022-11-14 16:10:29 +0200

    SP2

--- src/spot-checks/SpotCheck2.js
+++ src/spot-checks/SpotCheck2.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 export default function SpotCheck2() {
   // your code here
+  const [isLoading, setIsLoading] = useState(true);
+  const [lesson, setLesson] = useState({
+    name: "React Hooks",
+    completed: false,
+    started: false,
+  });
 
+  const updateLesson = (key) => {
+    setLesson({ ...lesson, [key]: true });
+  };
+
+  const renderLesson = () => {
+    return (
+      <div>
+        <p>{lesson.name}</p>
+        <p>
+          {lesson.completed
+            ? "Lesson complted"
+            : lesson.started
+            ? "In progress"
+            : "Not started"}
+        </p>
+      </div>
+    );
+  };
 
   return (
     <div className="ex-space">
       <h4 className="ex-title">Spot Check 2</h4>
       <div className="exercise" id="sc-2">
-        {/* your code here... */}
-
+        {isLoading ? <div>Page is loading...</div> : renderLesson()}
+        <button onClick={()=>updateLesson("started")}>Begin lesson</button>
+        <button onClick={()=>updateLesson("completed")}>Complete lesson</button>
       </div>
     </div>
-  )
+  );
 }
