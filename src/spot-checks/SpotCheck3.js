abd57ab3a18b27e114268ed2a6817612c8a4c035
Or <orlevy11@gmail.com>
2022-11-14 16:19:16 +0200

    SP3

--- src/spot-checks/SpotCheck3.js
+++ src/spot-checks/SpotCheck3.js
@@ -1,16 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function SpotCheck3() {
-  // your code here
+  const [likes, setLikes] = useState(0);
 
+  useEffect(() => {
+    document.title = `You have ${likes} likes`;
+  });
+
+  const addLike = () => setLikes(likes + 1);
 
   return (
     <div className="ex-space">
       <h4 className="ex-title">Spot Check 3</h4>
       <div className="exercise" id="sc-3">
-        {/* your code here... */}
-        
+        <p>My Post</p>
+        <button onClick={addLike}>Next page</button>
       </div>
     </div>
-  )
+  );
 }
