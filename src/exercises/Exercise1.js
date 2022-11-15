e4a699a3f2f7f77a2f1b9147fa7ec7b0689f057f
Or <orlevy11@gmail.com>
2022-11-15 12:23:13 +0200

    Ex 1,2

--- src/exercises/Exercise1.js
+++ src/exercises/Exercise1.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Exercise1() {
-  // your code here
+  const [username, setUsername] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
 
-
-  const countries = ['united states', 'china', 'united kingdom', 'australia']
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    name === "username"
+      ? setUsername(value)
+      : name === "city"
+      ? setCity(value)
+      : setCountry(value);
+  };
+  const countries = ["united states", "china", "united kingdom", "australia"];
   return (
     <div className="ex-space">
       <h4 className="ex-title">Exercise 1</h4>
       <div className="exercise" id="ex-1">
-        {/* your code here... */}
+        <input
+          value={username}
+          name="username"
+          onChange={handleChange}
+          placeholder="Username"
+        />
+        <input
+          value={city}
+          name="city"
+          onChange={handleChange}
+          placeholder="Your city"
+        />
 
+        <select name="country" onChange={handleChange} id="countries">
+          <option value={countries[0]}>{countries[0]}</option>
+          <option value={countries[1]}>{countries[1]}</option>
+          <option value={countries[2]}>{countries[2]}</option>
+          <option value={countries[3]}>{countries[3]}</option>
+        </select>
       </div>
     </div>
-  )
+  );
 }
--- src/exercises/Exercise2.js
+++ src/exercises/Exercise2.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Exercise2() {
-  // your code here
+  const [users, setUsers] = useState([]);
 
-
+  useEffect(async () => {
+    const usersFromApi = await axios.get(
+      "https://randomuser.me/api/?results=5"
+    );
+    setUsers(usersFromApi.data.results);
+  }, []);
   return (
     <div className="ex-space">
       <h4 className="ex-title">Exercise 2</h4>
       <div className="exercise" id="ex-2">
-        {/* your code here... */}
-
+        {users.map((u) => (
+          <div>
+            <div>{u.name.first}</div>
+            <div>{u.name.last}</div>
+            <img src={u.picture.thumbnail} />
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
